12c1f112a82436e9d62a9175d6068eb2e32fe012
andermuoz <a_munozber@fpzornotza.com>
2026-01-14 14:10:28 +0100

    commit con el carousel a medio hacer

--- LearningFactory/src/app/app.ts
+++ LearningFactory/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./components/footer/footer";
 import { Navbar } from "./components/navbar/navbar";
-import {CaracteristicasProducto} from "./components/caracteristicas-producto/caracteristicas-producto";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Navbar, CaracteristicasProducto],
+  imports: [RouterOutlet, Footer, Navbar, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- LearningFactory/src/app/components/home/home.ts
+++ LearningFactory/src/app/components/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  slides = [
+    {
+      title: 'LEARNING FACTORY',
+      text: 'Dale una segunda oportunidad al plástico. Recicla.',
+      image: 'img/RecycleC1.jpeg',
+    },
+    {
+      title: 'ECOLOGIA',
+      text: 'Creamos productos responsables con el planeta.',
+      image: 'img/recyclingC2.jpg',
+    },
+    {
+      title: 'SOSTENIBILIDAD',
+      text: 'El futuro empieza con pequeñas acciones.',
+      image: 'img/man-recycleC3.jpg',
+    },
+  ];
 
+  currentIndex = 0;
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
 }
