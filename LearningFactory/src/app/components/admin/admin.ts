632f6d511a78f4a531d1607eaddddf764679918e
andermuoz <a_munozber@fpzornotza.com>
2026-01-20 11:02:16 +0100

    commit con el servicio de usuario hecho

--- LearningFactory/src/app/components/admin/admin.ts
+++ LearningFactory/src/app/components/admin/admin.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Usuario } from '../../models/usuarios';
+import { Component, inject } from '@angular/core';
+import { Serviciousuario } from '../../services/serviciousuario';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -8,5 +11,31 @@ import { Component } from '@angular/core';
 })
 
 export class Admin {
+
+
+    private usersService = inject(Serviciousuario);
+
+    users: Usuario[] = [];
+    usuariosCategory: string[] = [];
+
+    usuariosFiltered: Usuario[] = [];
+
+    index: number = 0;
+
+    constructor(private router: Router) {
+    this.usersService.getUsuarios().subscribe({
+    next: (users) => {
+      console.log('USUARIOS =>', this.users);
+      this.users = users;
+      this.usuariosFiltered = users;
+      console.log('USUARIOS FILTRADOS =>', this.usuariosFiltered);
+      this.usuariosCategory = Array.from(
+        new Set(users.map((u: Usuario) => u.rol))
+      );
+    },
+    error: (err) => console.error(err)
+  });
+}
+
 
 }
