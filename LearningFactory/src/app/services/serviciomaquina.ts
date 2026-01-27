cab9e7488c2f1878c09e5b52dc7f9c311b130de7
gorka274 <g_kortabitartegon@fpzornotza.com>
2026-01-27 14:24:24 +0100

    Formulario compra hecho

--- LearningFactory/src/app/services/serviciomaquina.ts
+++ LearningFactory/src/app/services/serviciomaquina.ts
@@ -9,16 +9,20 @@ import { Maquina } from '../models/maquina';
 export class Serviciomaquina {
   private url = 'http://localhost:3000/MK677';
 
-      getMaquina(): Observable<any> {
-      return this.http.get(this.url);
-      }
+  getMaquina(): Observable<any> {
+    return this.http.get(this.url);
+  }
 
-      maquina: Maquina = {stock: 0, price: 0};
-      constructor(private http: HttpClient) {}
+  maquina: Maquina = {stock: 0, price: 0};
+  constructor(private http: HttpClient) {}
 
+  //Actualizar Stock y Precio de la máquina
+  updateStock(id: string, maquina: Maquina): Observable<Maquina> {
+    return this.http.put<Maquina>(`${this.url}/${id}`, maquina);
+  }
 
-      //Actualizar Stock maquina
-      updateStock(id: string, maquina: Maquina): Observable<Maquina> {
-        return this.http.put<Maquina>(`${this.url}/${id}`, maquina);
-      }
+  // Actualizar solo el stock (mantiene el precio sin cambios)
+  updateStockOnly(id: string, stock: number): Observable<Maquina> {
+    return this.http.patch<Maquina>(`${this.url}/${id}`, { stock });
+  }
 }
