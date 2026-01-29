57d63b9adc33662dc843319dc5aa55722d8cd30c
danda02 <d_arroyolop@fpzornotza.com>
2026-01-30 00:41:30 +0100

    SEO realizado

--- LearningFactory/src/app/components/caracteristicas-producto/caracteristicas-producto.ts
+++ LearningFactory/src/app/components/caracteristicas-producto/caracteristicas-producto.ts
@@ -1,24 +1,66 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { Serviciomaquina } from '../../services/serviciomaquina';
 import { Maquina } from '../../models/maquina';
 import { RouterLink } from "@angular/router";
+import { Title, Meta } from '@angular/platform-browser'; //SEO
 
 @Component({
   selector: 'app-caracteristicas-producto',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './caracteristicas-producto.html',
   styleUrl: './caracteristicas-producto.scss',
 })
-export class CaracteristicasProducto {
+export class CaracteristicasProducto implements OnInit {
   private servicio = inject(Serviciomaquina);
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
 
-  // Usamos un Signal para manejar el estado de forma reactiva
+  //Usamos un Signal
   datosMaquina = signal<Maquina | null>(null);
 
   constructor() {
     this.servicio.getMaquina().subscribe(data => {
+      const maquina = data[0];
       this.datosMaquina.set(data[0]); // Actualizamos el valor del signal
       console.log('Datos de la máquina:', data);
+
+      if (maquina) {
+        this.actualizarSEO(maquina);
+      }
+      
     });
   }
+
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Características de la Máquina MK677');
+  }
+
+  private actualizarSEO(maquina: any) {
+    //Titulo con el precio
+    this.titleService.setTitle(`Máquina MK677 - Comprar por ${maquina.price}€ | LearningFactory`);
+
+    //Descripción que incluye disponibilidad real del stock
+    this.metaService.updateTag({ 
+      name: 'description', 
+      content: `Adquiere la MK677 por solo ${maquina.price}€. Actualmente contamos con ${maquina.stock} unidades en stock. La mejor tecnología en reciclaje de plástico.` 
+    });
+
+    //Keywords específicas
+    this.metaService.updateTag({ 
+      name: 'keywords', 
+      content: `MK677 precio, comprar trituradora plástico, maquinaria reciclaje ${maquina.price} euros` 
+    });
+
+    //Redes Sociales
+    this.metaService.updateTag({ property: 'og:price:amount', content: maquina.price.toString() });
+    this.metaService.updateTag({ property: 'og:price:currency', content: 'EUR' });
+    this.metaService.updateTag({ 
+      property: 'og:description', 
+      content: `¡Oferta disponible! MK677 a ${maquina.price}€. Unidades disponibles: ${maquina.stock}.` 
+    });
+  }
+
+  
 }
